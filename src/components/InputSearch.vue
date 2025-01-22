4da59f03fb13c33fc334793a13dfaa7be8d522eb
HienB2105542 <hienb2105542@student.ctu.edu.vn>
2025-01-22 09:12:20 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    props: {
+        moduleValue: {type: String, default: ""},
+    },
+    emits: ["submit", "update:modeValue"],
+    methods: {
+        updateModeValue(e){
+            this.$emit("update:modeValue, e.target.value");
+        },
+        submit(){
+            this.$emit("submit");
+        },
+    },
+};
+</script>
+<template>
+    <div class="input-group">
+        <input
+        type="text"
+        class="form-control"
+        placeholder="Nhập thông tin cần tìm"
+        :value="moduleValue"
+        @input="updateModeValue"
+        @keyup.enter="submit" 
+        />
+        <div class="input-group-append">
+            <button
+            class="btn btn-ontline-secondary"
+            type="button"
+            @click="submit"
+            >
+            <i class="fas fa-search"></i> Tìm kiếm
+            </button>
+        </div>
+    </div>
+</template>
